dcf4fbe2a3bb0107d9e270aa4b5a51ff36e7068d
pankaj kumar <140154353+pankajkrg@users.noreply.github.com>
2024-10-20 12:47:13 +0530

    Update and rename jenkins_pipeline to main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "jenkins" {
+resource "aws_instance" "example" {
   ami           = "ami-0374badf0de443688"
   instance_type = "t2.micro"
 
